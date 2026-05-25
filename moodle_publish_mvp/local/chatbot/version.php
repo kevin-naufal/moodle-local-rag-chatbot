14b7870c46ecf716b2ba647d31a75da2c1be2420
Kevin Naufal Aryanto <kevinnaufal239@gmail.com>
2026-05-25 21:57:08 +0700

    Track evaluation data and remove stale Moodle copy

--- moodle_publish_mvp/local/chatbot/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->component = 'local_chatbot';
-$plugin->version   = 2026052201;
-$plugin->requires  = 2022112800; // Requires Moodle 4.1+ (based on version.php check earlier)
-$plugin->maturity  = MATURITY_ALPHA;
-$plugin->release   = 'v0.6.4';
