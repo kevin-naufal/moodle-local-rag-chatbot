00ecfb7266012be492fa5bd0ae72d712798e9818
Kevin Naufal Aryanto <kevinnaufal239@gmail.com>
2026-05-25 22:09:57 +0700

    Remove unused app utilities

--- app/list_models.py
+++
@@ -1,8 +0,0 @@
-import ollama
-with open('models.txt', 'w', encoding='utf-8') as f:
-    try:
-        models = ollama.list()['models']
-        for m in models:
-            f.write(m['model'] + '\n')
-    except Exception as e:
-        f.write(str(e))
